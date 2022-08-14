0d3fb7dd6091f545762206be0c1e1f56c501c2a5
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-08-14 16:32:14 -0500

    segimos el curso con prompt

--- 1-Variables/index.js
+++ 1-Variables/index.js
@@ -5,30 +5,32 @@ var num2 = 67;
 resultado = 0;
 // si no ponemos el tipo de variable al declararla, automaticamente se pondra Global.
 const num3 = 0;
-/*const: la variable no podra cambiar su valor una vez que la declaremos, esto se usa
-    regularmente para valores constantes qeu no nesecotemos que cambien a lo largo del programa
-    como seria PI 3.1416 y valores parecidos*/
+/*const: esta se decalra solo una vez esto quiere decir que no podra cambiar su valor una vez que sea declarada,
+esto se usa regularmente para valores constantes qeu no nesecotemos que cambien a lo largo del programa
+como seria PI 3.1416 y valores parecidos*/
 
 resultado = num1 + num2;
 document.write("El resultado de tu suma es:", resultado);
 
 /*
 JavaScript tiene tres tipos de declaraciones de variables.
-
 *var*
-Declara una variable, opcionalmente la inicia a un valor, y es de ambito global
-eso quiere decir que tiene acceso a todo el SCOPE.
-
+    Declara una variable, opcionalmente la inicia a un valor, y es de ambito global
+    eso quiere decir que tiene acceso a todo el SCOPE.
 *let*
-Declara una variable local con ámbito de bloque, opcionalmente la inicia a un valor.
-
+    Declara una variable local con ámbito de bloque, opcionalmente la inicia a un valor.
 *const*
-Declara un nombre de constante de solo lectura y ámbito de bloque. esto quiere decir que 
-desoues de declarada nunca mas se puede modificar su valor, estas se ocupan regularmente con valores
-fijos por ejemplo: PI 3.1416*/
-
-/* El contexto en el que los valores y las expresiones son "visibles" o pueden ser referenciados.
-    Si una variable u otra expresión no está "en el Scope- alcance actual", entonces no está disponible para su uso.*/
-
-/*Una variable declarada usando la instrucción var o let sin un valor asignado especificado
-tiene el valor de undefined.*/
+    const: es una variable constante de solo lectura y ámbito de bloque. esto quiere decir que 
+    despues de ser declarada nunca mas se puede modificar su valor, estas se ocupan regularmente con valores
+    fijos por ejemplo: PI 3.1416*/
+/*
+El contexto en el que los valores y las expresiones son "visibles" o pueden ser referenciados.
+    scope: Si una variable u otra expresión no está en el "Scope"  que significadentrode su alcance actual,
+    entonces no está disponible para su uso.
+    undefined: es Una variable declarada sin un valor asignado especificado
+    tiene el valor de undefined, osea no inicializada.
+    esto quiere decir que si existe en el programa pero que no se puede utilizar por que no tiene nada adentro
+    null: esat declarada e inicializada pero con un valor "nulo", oasea que hacemos que no tenga nada adentro intencionalmente
+    NaN: not a number "no es un numero", esto pasa regularmete cuando tratamos de hacer una operacion de variables con tipos de datos
+    de tipos de datos distintos, por ejemplo: tratar de multiplicar un String con un entero.*/
+    
--- 2-Prompt/index.js
+++ 2-Prompt/index.js
@@ -3,12 +3,15 @@ let name = prompt("what is your name?");
 document.write(
     `Hola "${name}" tu nombre se concateno con esta cadena de caracteres cuando lo ingresaste con "Prompt"`
 );
+alert("has ingresado esto: " + name);
 
-/*window.prompt()indica al navegador que muestre un cuadro de diálogo con un mensaje opcional que solicita
-al usuario que ingrese algún texto y que espere hasta que el usuario envíe el texto o cancele el cuadro de diálogo.
-
-En algunas condicisones, por ejemplo, cuando el usuario cambia de pestaña, es posible que el navegador no muestre
-un cuadro de diálogo o que no espere a que el usuario envíe el texto o cancele el cuadro de diálogo.*/
-
-/*NOTA: cabe mencionar que lo que se ingrese sera en cadena de texto eso quiere decir que se alojara en una variable
-de tipo "string" asi que si se ingresa un numero se tiene que "Parsear" para poder utilizarlo en operaciones aretmeticas*/
+/*
+*alert*
+    solo nos muestra algo, en un recuadro de dialogo (alerta).
+*prompt* 
+    muestra un cuadro de diálogo con un mensaje opcional que le demos
+    pidiendo datos al usuario spera hasta que el usuario ingrese algo o cancele la operacion.
+ATENCION: cabe mencionar que lo que se ingrese sera en cadena de texto eso quiere decir que se alojara en una variable
+    de tipo "string" asi que si se ingresa un numero se tiene que "Parsear" para poder utilizarlo en operaciones aretmeticas
+NOTA:En algunas condicisones, por ejemplo, cuando el usuario cambia de pestaña, es posible que el navegador no muestre
+    un cuadro de diálogo o que no espere a que el usuario envíe el texto o cancele el cuadro de diálogo.*/
