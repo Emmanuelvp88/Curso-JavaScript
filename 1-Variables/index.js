d4fcab29f3f18f701bb44080c7ede15c008b4d16
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-08-14 15:26:44 -0500

    segimos con el curso

--- 1-Variables/index.js
+++ 1-Variables/index.js
@@ -1,6 +1,13 @@
-let num1 = 34,
-    resultado = 0;
+let num1 = 34;
+// let: declaramos variables locales
 var num2 = 67;
+// var: decalramos variables Globales
+resultado = 0;
+// si no ponemos el tipo de variable al declararla, automaticamente se pondra Global.
+const num3 = 0;
+/*const: la variable no podra cambiar su valor una vez que la declaremos, esto se usa
+    regularmente para valores constantes qeu no nesecotemos que cambien a lo largo del programa
+    como seria PI 3.1416 y valores parecidos*/
 
 resultado = num1 + num2;
 document.write("El resultado de tu suma es:", resultado);
@@ -21,7 +28,7 @@ desoues de declarada nunca mas se puede modificar su valor, estas se ocupan regu
 fijos por ejemplo: PI 3.1416*/
 
 /* El contexto en el que los valores y las expresiones son "visibles" o pueden ser referenciados.
- Si una variable u otra expresión no está "en el Scope- alcance actual", entonces no está disponible para su uso.*/
+    Si una variable u otra expresión no está "en el Scope- alcance actual", entonces no está disponible para su uso.*/
 
 /*Una variable declarada usando la instrucción var o let sin un valor asignado especificado
 tiene el valor de undefined.*/
