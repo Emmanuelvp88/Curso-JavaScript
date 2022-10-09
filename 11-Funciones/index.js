a8b4b48a0c42ff9e465a4aa0e1dd5989d2977c3b
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-10-08 23:33:08 -0500

    Subimos leccion 11 Funciones

---
+++ 11-Funciones/index.js
@@ -0,0 +1,20 @@
+
+function saludar (nombre){
+    let frase = `!Hola¡ ${nombre} Como estas?`;
+    document.write(frase);
+}
+
+saludar("Emmanuel")
+
+
+function suma (){
+    let num1 = 23, num3 = 32; 
+    let res = num1 + num3;
+    return res;
+}
+
+let mostrar  = suma();
+document.write(mostrar);
+
+
+
