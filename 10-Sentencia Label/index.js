5bb6d5bbc04701ac67e75ba5e6fa169e321000ea
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-11-06 11:37:43 -0600

    terminamos leccion 11 funciones

--- 10-Sentencia Label/index.js
+++ 10-Sentencia Label/index.js
@@ -2,29 +2,39 @@ document.write(`<h2>Funcion Label</h2>`);
 /*El poner un array adentro de otro array es solo para indicar que en esa posicion entrara en accion
 un 'if' que contiene un 'for' que recorrera ese Array que esta dentro, no es como que 
 el array1 recorrera el array2.*/
-var mascotas = ["perro", "gato", "geco", mascotas2, "lagartija", ];
+var mascotas = ["perro", "gato", "geco", mascotas2, "lagartija", "brincolin"];
 var mascotas2 = ["walter", "hiena", "pequeña", "junior", "canela", "Princesa"];
 
 /* La sentencia "Label" es simplemente un nombre con el que podemos etiqeutar un bucle "for" y con ese
 nombre poder manipularlo mas facil */
 
-misMascotas: // Esta es un label con la  que podemos identificar este "for" principal
-for (let cachorros in mascotas) {
-    if(cachorros == 4){
+// "misMascotas" es un "label" con la  que podemos identificar este 'for' principal
+misMascotas: for (let cachorros in mascotas) {
+    if (cachorros == 5) {
+        //En este 'if' asemos uso del "label" 'misMascotas' para frenar el 'for' principal.
         break misMascotas;
     }
-    if (cachorros == 3) { // este "if" entra solo cuando el array "mascotas" llega a su posicion 3
+
+    if (cachorros == 3) {
+        // este "if" entra solo cuando el array "mascotas" llega a su posicion 3
         // En la variable "animales" guardamos el contenido de las pocisiones del array "nombreMascotas".
-        animales: //Esta es una "label" con la que vamos a identificar este "for" interno
-        for (let perros of mascotas2) {
-                //Recorremos el array directamente con la variable "animales" ya que en el "for of" alberga el contenido del array.
-                if (perros == "pequeña"){
-                    continue animales; //Con el "label" 'misMAscotas' Saltamos la iteracion del "for in" el for principal completo.
-                }
-                document.write(`${perros} ,`);
+
+        //animales es  la "label" con la que vamos a identificar este "for" interno
+        animales: for (let perros of mascotas2) {
+            //Recorremos el array directamente con la variable "animales" ya que en el "for of" alberga el contenido del array.
+            if (perros == "pequeña") {
+                continue animales; //Con el "label" 'misMAscotas' Saltamos la iteracion del "for in" el for principal completo.
             }
-    } else{
+            document.write(`${perros} ,`);
+        }
+    } else {
         //Recorremos el array con la variable "animales" ya que en el "for in" solo almacena el numero de las posiciones del array.
         document.write(` ${mascotas[cachorros]} ,`);
     }
 };
+
+document.write('<h2>Recorremos un array con un for normal</h2>');
+//De esta manera recorremos un 'array' normalmente con un 'for' normal.
+for(let i = 0; i < mascotas.length; i++){
+    document.write(`${mascotas[i]} ,`);
+}
--- 11-Funciones/index.js
+++ 11-Funciones/index.js
@@ -1,60 +1,61 @@
-/* "saludar" es el nombre de la funcion y se le pasa un parametro dentro de los parantesis "nombre" 
-    y posteriormente se define, sele da un valor, es como declarar una variable de forma local que solo funcionara dentro de estafuncion. */ 
+document.write(`<h2>Funcion 'saludar' con parametro</h2>`);
+/* "saludar" es el nombre de la funcion y se le pasa un parametro dentro de los parantesis y posteriormente se define
+cuando mandamos a llamar la funcion ahi es cuando sele da un valor de cualquier tipo, equivale a declarar una variable de forma local
+y solo funcionara dentro de la misma funcion. */
 function saludar(nombre) {
-    // aqui solo guardamos en la variable "frase" una cadena de texto junto con el parametro.
+    // aqui declaramos una variable "frase" donde alojaremos una cadena de texto agregando nuestro parametro.
     let frase = `!Hola¡ ${nombre} Como estas? <br>`;
-    //mandamos a imprimir la variable "frase"
+    //imprimimos la variable "frase"
     document.write(frase);
 }
-// Mandamos a llamar a la funcion "saludar" le pasamos el parametro pendiente por definir "nombre"
-// y ejecutara lo que tiene dentro junto con el parametro ya definido sera igual a esto: (nombre) = "Emmanuel"
-saludar("Emmanuel");
+/*Invocamos la  funcion "saludar" le pedimos el parametro pendiente por definir "nombre"
+y ejecutamos la funcion junto con el parametro ya definido sera igual a el nombre que ingrese el usuario mediante el "promt()"*/
+saludar(prompt("Hola cual es tu nombre"));
 
-
-/* en esta funcion "suma" declaramos unas variables las cuales las vamos a sumar y posteriormente 
-    las vamos a retornar con "return"*/
+document.write("<h2>Funcion 'Sumar' sin parametros</h2>");
+/*esta es una funcion normall donde no pasamos parametros y no retornamos nada, solo declaramos unas variables
+las cuales las vamos a sumar y posteriormente mostrar el resultado invocando la funcion*/
 function suma() {
     let num1 = 23;
     let num2 = 32;
-    let res = num1 + num2;
-    //retornamos la variable "res" que contiene el resultado de la suma
-    return res;
+    let resultado = num1 + num2;
+    document.write(`el resultado de tu suma es: ${resultado} <br>`);
 }
-//Guardamos la funcion "suma" en la varable "mostrar" la cual la mostramos econ un "document.write(mostrar)"
-//practicamente la funcion "suma" contiene lo retornado que viene siendo elresultado de la suma "res"
-let mostrar = suma();
-document.write("El resultado de tu suma es: " + mostrar  + "<br>");
+/*invocamos la funcion "suma"  qe contiene lo toda la operacion y gracias al 'document.write()'
+que tiene dentro la funcion podemos visualizar el resultado*/
+suma();
 
-
-//Hay otra manera de declarara las funciones, es poner primero el nombre de la funcion  despues declararla
+document.write(`<h2>Otra manera de declarar funciones, Funcion 'comparar'</h2>`);
+//Esat es otra manera de declarara las funciones, es poner primero el nombre de la funcion  despues declararla
 iguales = function (x, y) {
-    //Comparamos los parametros dentro de la funcion "x,y" y guardamos el resultado dentro de la variable "resul"
-    let comparar = x==y;
+    //Comparamos los parametros "x,y" dentro de la funcion y guardamos el resultado dentro de la variable "comparar"
+    let comparar = x == y;
     //La cual retornamos, dandonos como resultado un valor voleano
     return comparar;
-}
-// La funcion la guardamos dentro de una variable "resultado" y  El resultado de la funcion sera lo que retornemos
-// y dependera de los valores que le demos a los parametros dentro de la funcion "iguales(8,4)"
-let resultado = iguales(8, 4);
+    /*El 'return' nos sirve para retornar un resultado y el cual tambien actua como un 'break' porque lo que venga
+        despues de el ya no se ejecutara.*/
+};
+/*La funcion la guardamos dentro de una variable "resultado" la cual mostraremos a continuacion con un 'document.write()' 
+    dependiendo los valores que le mandemos sera el resultado, en este caso comparamos si 8 */
+let resultado = iguales("4", 4);
 //Mostramos el resultado imprimindo la variable "resultado" que contiene a la funcion misma.
 document.write(`El resultado de tu Comparacion es: ${resultado} <br>`);
 
-
+document.write(`<h2>Funciones flecha</h2>`);
 /*-------------Funciones Flecha--------- */
 //Son las que vamos a utilizar de ahora en adelante.
 
-/*La ventaja de las funciones tipo flecha esque cuando solo tenemos un parametro, no es necesario 
-    encerrarla en parentesis y funcionan de igual forma*/
-const saludo = nombre=>{
-    document.write(`Hola como estas ${nombre} <br>`);
-}
-saludo("Emamnuel");
+/*La ventaja de las funciones tipo flecha esque cuando solo tenemos un parametro 'nombre' no es necesario
+    encerrarlo en parentesis y cundo tenemos solo una exprecion o linea de codigo en el cuerpo de la funcion
+    podemos no poner las llavez  {} ademas que todo se puede poner en una sola linea y se retorna automaticamente*/
+const saludo = nombre => document.write(`Hola como estas ${nombre} <br>`);
+saludo("Vato loco");
 
-
-//Cuando tenemos mas de un parametro si se necesitan encerrar entre parantesis.
-const resta = (num1, num2)=>{
-    let resultado = num1 -num2;
+//Cuando tenemos mas de un parametro si se necesitan encerrar entre parantesis y en el cuerpo poner todo entre llavez.
+const resta = (num1, num2) => {
+    let resultado = num1 - num2;
     return resultado;
-}
-// let respuesta = resta(10 ,5);
-document.write(`El resultado de tu resta es: ${resta(10, 4)}`);
+};
+// Mostramos la funcion dentro del mismo 'document.write()' por ende se muestra lo que retornamos
+document.write(`El resultado de tu resta es: ${resta(10, 4)} <br>`);
+resta(8, 3);
