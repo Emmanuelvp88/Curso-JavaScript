2018560c8fcb43a1844e4bee4b2d8b7f1a1e031c
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-04 12:08:59 -0600

    creamosla prier clase de  POO

--- 13-POO/index.js
+++ 13-POO/index.js
@@ -1,1 +1,17 @@
-alert("que tarnsita");
+class Celulares {
+    propiedades(tamaño, resolucion, camara, ram) {
+        this.tamaño = tamaño;
+        this.resolucion = resolucion;
+        this.camara = camara;
+        this.ram = ram;
+    }
+
+    infoCel1() {
+        document.write(`el celulra1 es de: ${this.tamaño},
+            ${this.resolucion},${this.camara}, ${this.ram}`);
+    }
+}
+
+/**Creamos un nuevo objeto en donde vamos a pasar todos los parametros */
+const celulra1 = new Celulares("10'", "ful hd", "20mx", "2gb");
+celulra1.infoCel1();
