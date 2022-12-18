9c84c4f1b1155cf1d57ca046691541870865c4ab
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-18 17:00:02 -0600

    Cambiamos las clase celulares

--- 13-POO/index.js
+++ 13-POO/index.js
@@ -1,3 +1,5 @@
+
+//clase padre "Celulares"
 class Celulares {
     /**Este es el constructor que va recibir los parametros del objeto */
     constructor(dimencion, resolucion, camara, ram) {
@@ -5,15 +7,45 @@ class Celulares {
         this.resolucion = resolucion;
         this.camara = camara;
         this.ram = ram;
-        // this.info = ;
-    }
+        // this.info = this.dimencion, this.camara, this.ram, this.resolucion ;
+        // this.encendido = true;
 
-    infoCel1() {
-        document.write(`el celulra1 es de: ${this.dimencion},
-        ${this.resolucion},${this.camara}, ${this.ram}`);
+    }
+    // encender(){
+    //     if(this.encendido == true){
+    //         alert("el celular esta encendiendo");
+    //         // this.encendido = false;
+    //     }else{
+    //         alert("el celular esta apagado");
+    //     }
+    // }
+    // reiniciar(){
+    //     if(this.encendido == true){
+    //         document.write("El celular se esta reiniciando...");
+    //         this.encendido = false;
+    //     }else{
+    //         document.write("El celular esta apagado");
+    //     }
+    // }
+    // tomarFoto(){
+    //     if(){
+
+    //     }
+    // }
+
+    infoCel() {
+        // document.write(`el celulra es de: ${this.dimencion},
+        // ${this.resolucion},${this.camara}, ${this.ram}`);
+        // document.write(this.info);
     }
 }
 
 /**Creamos un nuevo objeto en donde vamos a pasar todos los parametros */
-const celulra1 = new Celulares("10'", "ful hd", "20mx", "2gb");
-celulra1.infoCel1();
+const celulra1 = new Celulares("5'", "ful hd", "50mx", "6gb");
+const celulra2 = new Celulares("5.5'", "hd", "30mx", "2gb");
+const celulra3 = new Celulares("4.3'", "ultra hd", "70mx", "6gb");
+
+document.write(`${celulra1.infoCel()}`)
+document.write(`${celulra2.infoCel()}`)
+document.write(`${celulra3.infoCel()} <br>`)
+
