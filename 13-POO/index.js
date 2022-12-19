ad4c0f1d682801742cd2ba048b7690fd3907b795
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-18 18:40:05 -0600

    Agregamos todos llos metodos a la clase celular

--- 13-POO/index.js
+++ 13-POO/index.js
@@ -1,4 +1,3 @@
-
 //clase padre "Celulares"
 class Celulares {
     /**Este es el constructor que va recibir los parametros del objeto */
@@ -7,45 +6,54 @@ class Celulares {
         this.resolucion = resolucion;
         this.camara = camara;
         this.ram = ram;
-        // this.info = this.dimencion, this.camara, this.ram, this.resolucion ;
-        // this.encendido = true;
-
+        this.encendido = false;
     }
-    // encender(){
-    //     if(this.encendido == true){
-    //         alert("el celular esta encendiendo");
-    //         // this.encendido = false;
-    //     }else{
-    //         alert("el celular esta apagado");
-    //     }
-    // }
-    // reiniciar(){
-    //     if(this.encendido == true){
-    //         document.write("El celular se esta reiniciando...");
-    //         this.encendido = false;
-    //     }else{
-    //         document.write("El celular esta apagado");
-    //     }
-    // }
-    // tomarFoto(){
-    //     if(){
-
-    //     }
-    // }
+    botonEncender() {
+        if (this.encendido == false) {
+            alert("el celular esta encendiendo...");
+            this.encendido = true;
+        } else {
+            alert("el celular esta apagando...");
+        }
+    }
+    reiniciar() {
+        if (this.encendido == true) {
+            alert("El celular se esta reiniciando...");
+        } else {
+            alert("El celular esta apagado");
+        }
+    }
+    tomarFoto() {
+        if (this.encendido == true) {
+            alert("La foto se atomado en una resolucion de: " + this.resolucion);
+        }
+    }
+    brabarVideo(){
+        if(this.encendido == true){
+            alert("Grabando video en resolucion: " + this.resolucion);
+        }
+    }
 
     infoCel() {
-        // document.write(`el celulra es de: ${this.dimencion},
-        // ${this.resolucion},${this.camara}, ${this.ram}`);
-        // document.write(this.info);
+       return document.write(`
+        el celulra es de:<b>${this.dimencion}</b> <br>
+        Resulucion de: :<b>${this.resolucion}</b> <br>
+        Tien memoria ram de: <b>${this.ram}</b> <br>
+        tiene una camara de: <b>${this.camara}</b> <br><br>
+        
+        `);
     }
 }
-
 /**Creamos un nuevo objeto en donde vamos a pasar todos los parametros */
 const celulra1 = new Celulares("5'", "ful hd", "50mx", "6gb");
 const celulra2 = new Celulares("5.5'", "hd", "30mx", "2gb");
 const celulra3 = new Celulares("4.3'", "ultra hd", "70mx", "6gb");
 
-document.write(`${celulra1.infoCel()}`)
-document.write(`${celulra2.infoCel()}`)
-document.write(`${celulra3.infoCel()} <br>`)
+celulra1.botonEncender();
+celulra1.reiniciar();
+celulra1.brabarVideo();
+celulra1.tomarFoto();
 
+celulra1.infoCel();
+celulra2.infoCel();
+celulra3.infoCel();
