dbffd2b41af91c401469cd58839b4dabbb5783bb
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-21 23:00:37 -0600

    Subimos el metodo de mostrar info

--- 13-POO/app.js
+++ 13-POO/app.js
@@ -7,23 +7,41 @@ class App{
         this.appInstalada = false;
     }
     instalar(){
-        this.appInstalada = true;
-        if(this.appInstalada == true){
+        if(this.appInstalada == false){
             alert("la app esta instalandose...");
+            this.appInstalada = true;
         }
-    };
+    }
+    iniciarApp(){
+        if(this.appIniciada == false & this.appInstalada == true){
+            alert("app se esta iniciando...");
+            this.appIniciada = true;
+        }
+    }
+    cerrarApp(){
+        if(this.appIniciada == true & this.appInstalada == true){
+            alert("cerrando app...");
+        }
+    }
     desistalar(){
         if(this.appInstalada == true){
             alert("la app se esta desistalando...");
             this.appInstalada = false;
         }
+    }
+    infoApp(){
+        return ` 
+            Numero de descargas: <b>${this.descargas}</b><br>
+            Calificacion: <b>${this.puntuacion}</b><br>
+            Pesa: <b>${this.peso}</b><br><br>
+        `
     };
-    iniciarApp(){
-        if(this.appIniciada == false & this.appInstalada == true){
-            alert("app se esta iniciando...");
-        }
-    };
-
-
-
 }
+
+var app1 = new App("24", "5 estrellas", "34mb");
+
+// app1.instalar();
+    document.write(app1.infoApp())
+
+
+
