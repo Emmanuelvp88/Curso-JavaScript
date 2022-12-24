8ecce368de19301aa328f764036cf6135e56d49c
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-24 17:01:09 -0600

    Quitamos el defer en el archivo hTML ya que metia un problema de ejecuacuon con en docuemnt.write

--- 13-POO/app.js
+++ 13-POO/app.js
@@ -16,17 +16,23 @@ class App{
         if(this.appIniciada == false & this.appInstalada == true){
             alert("app se esta iniciando...");
             this.appIniciada = true;
+        }else{
+            alert("App no pude iniciar porque no esta instalada");
         }
     }
     cerrarApp(){
         if(this.appIniciada == true & this.appInstalada == true){
             alert("cerrando app...");
+        }else{
+            alert("La app no puede ser cerrada, porque no esta iniciada o no esta instalada");
         }
     }
     desistalar(){
         if(this.appInstalada == true){
             alert("la app se esta desistalando...");
             this.appInstalada = false;
+        }else{
+            alert("la app no pusde ser desistalada porque no esta instalada");
         }
     }
     infoApp(){
@@ -35,13 +41,16 @@ class App{
             Calificacion: <b>${this.puntuacion}</b><br>
             Pesa: <b>${this.peso}</b><br><br>
         `
-    };
+    }
 }
 
-var app1 = new App("24", "5 estrellas", "34mb");
 
+
+var app1 = new App("24", "5 estrellas", "34mb");
 // app1.instalar();
-    document.write(app1.infoApp())
+// app1.iniciarApp();
+
+document.write(app1.infoApp());
 
 
 
