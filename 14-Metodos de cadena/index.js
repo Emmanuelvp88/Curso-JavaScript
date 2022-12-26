999a9bbbc2f1d3949cc68896e67c54100f508dbd
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-25 18:45:23 -0600

    terminamos de subir el concat

--- 14-Metodos de cadena/index.js
+++ 14-Metodos de cadena/index.js
@@ -1,4 +1,10 @@
-let cadenaPrueba = "Hola que onda: ";
-let resultado = cadenaPrueba.concat("Emamnuel");
+//metodo "concat" concatena algo extra a una cadena string ya declarada
+let cadPrueba = "Hola que onda: ";
+let resultado = "";
 
+//"concat" es igial que concatenar con un '+' una cadena a uan variable string ya declarada
+resultado = cadPrueba.concat("Villalva <br>");
+document.write(resultado );
+
+resultado = cadPrueba + "Emamnuel <br>";//equivalente a usar "concat"
 document.write(resultado);
