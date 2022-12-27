eed85f228b71fa5474e80ec1c1f3e0cb45ecbaed
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-26 23:45:38 -0600

    subimso el 4to metodo includes

--- 14-Metodos de cadena/index.js
+++ 14-Metodos de cadena/index.js
@@ -16,7 +16,16 @@ let comparar = "Prob";
 resultado = cadPrueba.startsWith(comparar)
 document.write(`¿"cadPrueba" empieza con la misma linea de caracteres, que la cadena "comparar"?: ${resultado} <br>`);
 
-/**"endsWith" se encarga de comprarar que los ultimos caracteres de una cadea X sea igual a lso ultimos de otra cadena X */
+/**"endsWith" se encarga de comprarar que los ultimos caracteres de una cadea X sea igual a lso ultimos de otra cadena X
+    y devuelve "true" si lso caracteres son los mismos  o "false" si no.*/
 comparar = "odo";
 resultado = cadPrueba.endsWith(comparar);
-document.write(`¿cadPrueba termona con la misma linea de cararteres que la cadena "comparar"?: ${resultado} <br>`);
+document.write(`¿cadPrueba termina con la misma linea de cararteres que la cadena "comparar"?: ${resultado} <br>`);
+
+/* "includes" comprueba que una cadena se encuentre incluida dentro de otra cadena "X", y si se encuentra incluida
+nos regresa "true" de lo contrario nos devuelve "false"*/
+
+cadPrueba = "Emmanuel sera un programador exitoso"
+comparar = "un"
+resultado = cadPrueba.includes(comparar);
+document.write(`la cadena "<b>${comparar}</b>" se encuentra incluida en la cadena "<b>${cadPrueba}</b>"?: ${resultado}`);
