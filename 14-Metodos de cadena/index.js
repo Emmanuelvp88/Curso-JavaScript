b2dc4d9553c6342c16160388f530b4299e7fbc2f
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-29 15:54:58 -0600

    agregamos unos comentarios en cada metodo

--- 14-Metodos de cadena/index.js
+++ 14-Metodos de cadena/index.js
@@ -1,12 +1,12 @@
 //metodo "concat" concatena algo extra a una cadena string ya declarada
-let cadPrueba = "Hola que onda: ";
+let cadPrueba = "Hola que onda soy Emmanuel Villalva y sere un gran desarrollador";
 let resultado = "";
 
 //"concat" es igial que concatenar con un '+' una cadena a uan variable string ya declarada
-resultado = cadPrueba.concat("Villalva <br>");
-document.write(resultado );
+resultado = cadPrueba.concat(" Backend<br>");
+document.write(`<b>concat: </b> ${resultado}` );
 
-resultado = cadPrueba + "Emamnuel <br>";//equivalente a usar "concat"
+resultado = cadPrueba + " Backend<br>";//equivalente a usar "concat"
 document.write(resultado);
 
 /*"startWith" Compara que una cadena empiece con los mismos caracteres que otra y regresara
@@ -14,23 +14,23 @@ document.write(resultado);
 cadPrueba = "Probamos el segundo metodo"
 let comparar = "Prob";
 resultado = cadPrueba.startsWith(comparar)
-document.write(`¿"cadPrueba" empieza con la misma linea de caracteres, que la cadena "comparar"?: ${resultado} <br>`);
+document.write(`<b>startWith: </b>¿"cadPrueba" empieza con la misma linea de caracteres, que la cadena "comparar"?: ${resultado} <br>`);
 
 /**"endsWith" se encarga de comprarar que los ultimos caracteres de una cadea X sea igual a lso ultimos de otra cadena X
     y devuelve "true" si lso caracteres son los mismos  o "false" si no.*/
 comparar = "odo";
 resultado = cadPrueba.endsWith(comparar);
-document.write(`¿La cadena "<b>${cadPrueba}</b>" termina con la misma linea de cararteres que la cadena "<b>${comparar}</b>"?: ${resultado} <br>`);
+document.write(`<b>endWith: </b>¿La cadena "<b>${cadPrueba}</b>" termina con la misma linea de cararteres que la cadena "<b>${comparar}</b>"?: ${resultado} <br>`);
 
 /* "includes" comprueba que una cadena se encuentre incluida dentro de otra cadena "X", y si se encuentra incluida
 nos regresa "true" de lo contrario nos devuelve "false"*/
 cadPrueba = "Emmanuel sera un programador exitoso"
 comparar = "un"
 resultado = cadPrueba.includes(comparar);
-document.write(`La cadena "<b>${comparar}</b>" se encuentra incluida en la cadena "<b>${cadPrueba}</b>"?: ${resultado}<br>`);
+document.write(`<b>includes: </b>La cadena "<b>${comparar}</b>" se encuentra incluida en la cadena "<b>${cadPrueba}</b>"?: ${resultado}<br>`);
 
 
-/*"indexOf" */
+/*"indexOf" nos indica el indice de donde empieza alguna cadena de caracteres */
 
-resultado = cadPrueba.indexOf("e");
-document.write(`la palabra  empieza en el caracter numero: ${resultado}`);
+resultado = cadPrueba.indexOf("programador");
+document.write(`<b>indexOf: </b>El index empieza en el caracter numero: ${resultado}`);
