0ac5274c52139e76c9f24e71aa5252c7c2fc7c5e
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-01-01 22:08:20 -0600

    cambie nuevamente el comentario de Upper y lower

--- 14-Metodos de cadena/index.js
+++ 14-Metodos de cadena/index.js
@@ -57,11 +57,11 @@ document.write(`<b>substring: </b> del caracter 36 al 77 de la cadena <b>${cadPr
 
 /*"ToLowerCase" este metodo convierte toda una cadena en minusculas */
 resultado = cadPrueba.toLowerCase();
-document.write(`<b>lowerCase:</b> Todas las mayusculas de esta cadena fueraon convertidas a minusculas: ${resultado} <br>`);
+document.write(`<b>ToLowerCase:</b> Todas las mayusculas de esta cadena fueraon convertidas a minusculas: ${resultado} <br>`);
 
 /*"toUpperCase" este metodo nos convierte todas las minusculas a mayusculas de una cadena*/
 resultado = cadPrueba.toUpperCase();
-document.write(`<b>lowerCase:</b> Todas las minsculas de esta cadena fueraon convertidas a mayusculas: ${resultado} <br>`);
+document.write(`<b>toUpperCase:</b> Todas las minsculas de esta cadena fueraon convertidas a mayusculas: ${resultado} <br>`);
 
 /*"toString" este metodo nos cinvierte cualquier tipo de varible, array o cualquier otra cosa a una cadena de texto */
 let cadena = 380
