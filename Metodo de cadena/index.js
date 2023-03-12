d15a0c783fbddd2f5012eb9a4b29e17adb68d0bd
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-03-12 11:27:58 -0600

    Asindo la calculadora nuevamente

--- Metodo de cadena/index.js
+++ Metodo de cadena/index.js
@@ -1,27 +1,5 @@
-let cantidad = prompt("Hola dime cuantos alumnos son?");
-const alumnosTotales = [];
-// Guardando el nombr de los alumnos
-for (let i = 0; i < cantidad; i++) {
-    alumnosTotales[i] = [prompt(`Dame el nombre del alumno numero ${i + 1}`), 0]
-}
-
-const tomarAsistencia = (nombre, p) => {
-    let asistencia = prompt(`Pon "p" si el alumno ${nombre} estubo presente el dia ${i + 1}`);
-    if(asistencia == "p" || asistencia == "P"){
-        alumnosTotales[p][1]++;
-    };
-};
-
-for (i = 0; i < 30; i++){
-    for (alumno in alumnosTotales){
-        tomarAsistencia(alumnosTotales[1][0], alumno);
-    }
-}
-
-for(alumno in alumnosTotales){
-    let resultado = `El alumno ${alumnosTotales[alumno][0]} tiene: <br>
-    Asistencias: ${alumnosTotales[alumno][1]}<br>
-    faltas: ${30 - alumnosTotales[alumno][1]}<br>
-    `
-    document.write(resultado);
-}
+operacion = prompt(`Hola Bienvenido a la calculadora de Emmanuel, Dime que operacion deseas realizar
+                1.- suma
+                2.- Resta
+                3.- divicion 
+                4.- Multiplicacion`);
