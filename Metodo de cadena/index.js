9a8991b308776fab37cd245936eb8019a8e30d38
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-03-12 10:56:13 -0600

    seguimos en practica

--- Metodo de cadena/index.js
+++ Metodo de cadena/index.js
@@ -1,6 +1,15 @@
 let cantidad = prompt("Hola dime cuantos alumnos son?");
 const alumnosTotales = [];
-
-for (let i = 0; i < cantidad; i++){
-    alumnosTotales [i] = [prompt(`Dame el nombre del alumno numero ${i+1}`), 0]
+// Guardando el nombr de los alumnos
+for (let i = 0; i < cantidad; i++) {
+    alumnosTotales[i] = [prompt(`Dame el nombre del alumno numero ${i + 1}`), 0]
 }
+
+const tomarAsistencia = (nombre, p) => {
+    let asistencia = prompt(`Pon "p" si el alumno ${nombre} estubo presente el dia ${i + 1}`);
+    if(asistencia == "p" || asistencia == "P"){
+        alumnosTotales[p][1]++;
+    }
+}
+
+
