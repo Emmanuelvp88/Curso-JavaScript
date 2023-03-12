3eab01862a918ba93f97bbc20483a0c70c846724
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-03-12 10:00:33 -0600

    problamas con amulacion de programa

--- Metodo de cadena/index.js
+++ Metodo de cadena/index.js
@@ -5,7 +5,7 @@ for(i = 0; i < cantidad; i++){
     alumnosTotales = [i][prompt(`Dame el nombre del alumno numero ${i+1}`), 0];
 }
 const tomarAsistencia = (nombre, p) =>{
-    presente = prompt(`Escribe "p" si el alumno ${nombre} estubo presente el dia ${alumno+1}`);
+    let presente = prompt(`Escribe "p" si el alumno ${nombre} estubo presente el dia`);
     if (presente == "p" || presente == "P"){
         alumnosTotales[p][1]++;
     }
