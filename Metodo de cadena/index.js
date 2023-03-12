7c57831c7cda7b1e72c652ac6a00826207e7124d
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-03-12 11:20:56 -0600

    comcluimos con la practica

--- Metodo de cadena/index.js
+++ Metodo de cadena/index.js
@@ -9,7 +9,19 @@ const tomarAsistencia = (nombre, p) => {
     let asistencia = prompt(`Pon "p" si el alumno ${nombre} estubo presente el dia ${i + 1}`);
     if(asistencia == "p" || asistencia == "P"){
         alumnosTotales[p][1]++;
+    };
+};
+
+for (i = 0; i < 30; i++){
+    for (alumno in alumnosTotales){
+        tomarAsistencia(alumnosTotales[1][0], alumno);
     }
 }
 
-
+for(alumno in alumnosTotales){
+    let resultado = `El alumno ${alumnosTotales[alumno][0]} tiene: <br>
+    Asistencias: ${alumnosTotales[alumno][1]}<br>
+    faltas: ${30 - alumnosTotales[alumno][1]}<br>
+    `
+    document.write(resultado);
+}
