cdfdd223dc28ed70bdfb23768e11439ec36833ab
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-03-12 10:43:46 -0600

    Probando nueamente prueba numero 3

--- Metodo de cadena/index.js
+++ Metodo de cadena/index.js
@@ -1,31 +1,6 @@
-cantidad = prompt("Hola cuantos alumnos son?");
-alumnosTotales = [];
+let cantidad = prompt("Hola dime cuantos alumnos son?");
+const alumnosTotales = [];
 
-for(i = 0; i < cantidad; i++){
-    alumnosTotales[i] = [prompt(`Dame el nombre del alumno numero ${i+1}`), 0];
-}
-const tomarAsistencia = (nombre, p) =>{
-    let presente = prompt(`Escribe "p" si el alumno ${nombre} estubo presente el dia`);
-    if (presente == "p" || presente == "P"){
-        alumnosTotales[p][1]++;
-    }
-}
-for(i = 0; i < 30; i++){
-    for (alumno in alumnosTotales){
-        tomarAsistencia(alumnosTotales[alumno][0], alumno);
-    }
-}
-
-for(alumno in alumnosTotales){
-    let resultado = `El alumno ${alumnosTotales[alumno][0]} tiene: <br>
-    Asistencias: ${alumnosTotales[alumno][1]}<br>
-    Faltas: ${30 - alumnosTotales[alumno][1]}<br>
-    ` 
-
-    if(alumnosTotales[alumno][1] > 18){
-        resultado += `Por lo tanto el alumno ${alumnosTotales[alumno][0]} esta <b>Aprobado</b> <br>`
-    }else{
-        resultado += `Por lo tanto el alumno ${alumnosTotales[alumno][0]} esta <b>Reprobado</b> <br>`
-    }
-    document.write(resultado);
+for (let i = 0; i < cantidad; i++){
+    alumnosTotales [i] = [prompt(`Dame el nombre del alumno numero ${i+1}`), 0]
 }
