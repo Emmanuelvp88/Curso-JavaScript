0073edcebddfad9f3164254c2ea416b0a3c44414
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-04-01 16:47:38 -0600

    Sibi metodo pop

--- 15.-Metodos de Array/index.js
+++ 15.-Metodos de Array/index.js
@@ -1,12 +1,13 @@
 //Pop
 document.write("<b>pop: </b> quita el ultimo elemento de un array y lo almacena en una variable <br>");
 let array = ["jorge ", " pedro ", " juan"]
-document.write(`Asi se ve el array normalmente: <b>${array}</b> <br>`);
+document.write(`Array normalmente: <b>${array}</b> <br>`);
 array.pop();
-document.write(`Asi se ve el array despues del metodo "pop": <b> ${array}</b> <br>`);
+document.write(`Array despues del metodo "pop": <b> ${array}</b> <br>`);
 let ultimoElemento = array.pop();
-document.write(`Ahora mostramos el ultimo elemeto del array asignandolo a una variable: <b>${ultimoElemento}</b><br>`);
-document.write(`ahora mostraremos como quedo el array al final de todo: <b>${array}</b>`)
+document.write(`Mostramos el ultimo elemeto del array asignandolo a una variable: <b>${ultimoElemento}</b><br>`);
+document.write(`Mostraremos como quedo el array al final de todo: <b>${array}</b>`)
 //Recordemos que el metodo "pop" va quitando el ultimo elemento del array como actualmente esta
 //Es por eso que al final de todo el array solo nos quedo con un solo elemento
 
+document.write(``);
