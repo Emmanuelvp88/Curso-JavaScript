15ff6de875d421788e116a9ce0ec43c8d904d3d4
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-04-16 16:15:27 -0600

    elemento Math

--- 16.-Objeto Math - Basico/index.js
+++ 16.-Objeto Math - Basico/index.js
@@ -1,1 +1,4 @@
-prompt(`dasnodnoiasdnposas`)
+// --------------Objeto MATH--------------
+let resultado;
+document.write(`<b>Math.sqrt:</b> Nos mustra la raiz cuadrada de un numero. raiz cuadrada de 25 = <b>${resultado = Math.sqrt(25)}</b><br>`);
+document.write(`<b>Math.</b>`);
