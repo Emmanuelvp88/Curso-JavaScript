3f0ce20f6a6022479f1b8694d1046c2b74002b33
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-04-16 16:59:48 -0600

    Agrege el .raund de Math

--- 16.-Objeto Math - Basico/index.js
+++ 16.-Objeto Math - Basico/index.js
@@ -1,4 +1,7 @@
 // --------------Objeto MATH--------------
 let resultado;
 document.write(`<b>Math.sqrt:</b> Nos mustra la raiz cuadrada de un numero. raiz cuadrada de 25 = <b>${resultado = Math.sqrt(25)}</b><br>`);
-document.write(`<b>Math.</b>`);
+document.write(`<b>Math.cbrt:</b> Nos muestra la raiz cubica de un numero. Raiz cubica de 35 = ${resultado = Math.cbrt(35)}<br>`);
+document.write(`<b>Math.max:</b> Nos devuelde el numero mayor, y solo recibe puros numeros. El numero mayor (3,34,54,67,12,2342) = ${resultado = Math.max(3,34,54,67,12,342)}<br>`);
+document.write(`<b>Math.min:</b> Nos devuelde el numero menor, y solo recibe puros numeros. El numero menor (3,34,54,67,12,2342) = ${resultado = Math.min(3,34,54,67,12,342)}<br>`);
+document.write(`<b>Matah.random:</b> Nos da un numero aleatoria del entre el 0 y 1.  Nmeros random entre 1 y 100 = ${resultado = Math.random() * 100}`);
