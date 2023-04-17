9e847aad4b694428744f0b96bb494c0683dc1612
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-04-16 21:30:24 -0600

    Agrege otras del objeto math

--- 16.-Objeto Math - Basico/index.js
+++ 16.-Objeto Math - Basico/index.js
@@ -1,7 +1,11 @@
 // --------------Objeto MATH--------------
 let resultado;
-document.write(`<b>Math.sqrt:</b> Nos mustra la raiz cuadrada de un numero. raiz cuadrada de 25 = <b>${resultado = Math.sqrt(25)}</b><br>`);
-document.write(`<b>Math.cbrt:</b> Nos muestra la raiz cubica de un numero. Raiz cubica de 35 = ${resultado = Math.cbrt(35)}<br>`);
-document.write(`<b>Math.max:</b> Nos devuelde el numero mayor, y solo recibe puros numeros. El numero mayor (3,34,54,67,12,2342) = ${resultado = Math.max(3,34,54,67,12,342)}<br>`);
-document.write(`<b>Math.min:</b> Nos devuelde el numero menor, y solo recibe puros numeros. El numero menor (3,34,54,67,12,2342) = ${resultado = Math.min(3,34,54,67,12,342)}<br>`);
-document.write(`<b>Matah.random:</b> Nos da un numero aleatoria del entre el 0 y 1.  Nmeros random entre 1 y 100 = ${resultado = Math.random() * 100}`);
+document.write(`<b>Math.sqrt:</b> Muestra la raiz cuadrada de un numero. raiz cuadrada de 25 = <b>${resultado = Math.sqrt(25)}</b><br>`);
+document.write(`<b>Math.cbrt:</b> Mueestra la raiz cubica de un numero. Raiz cubica de 35 = ${resultado = Math.cbrt(35)}<br>`);
+document.write(`<b>Math.max:</b> Devuelde el numero mayor, y solo recibe puros numeros. El numero mayor (3,34,54,67,12,2342) = ${resultado = Math.max(3,34,54,67,12,342)}<br>`);
+document.write(`<b>Math.min:</b> Devuelde el numero menor, y solo recibe puros numeros. El numero menor (3,34,54,67,12,2342) = ${resultado = Math.min(3,34,54,67,12,342)}<br>`);
+document.write(`<b>Matah.random:</b> Da un numero aleatoria del entre el 0 y 1.  Nmeros random entre 1 y 100 = ${resultado = Math.random() * 100}<br>`);
+document.write(`<b>Math.round:</b> Redondea el numero decimal a el numero mas cercano ya sea para arriba o para abajo. Ejemplo 3.56767 = ${resultado = Math.round(3.567856)}<br>`);
+document.write(`<b>Math.floor:</b>Redondea los numeros decimales hacia abajo. Ejemplo: 3.98889 = ${resultado = Math.floor(3.9988)} <br>`)
+document.write(`<b>Math.fround:</b>Redondea los decimales de un una cifra que contenga decimales o variable foltante. Ejemplo: 3.6056999999999 = ${resultado = Math.fround(3.605699999999999999)}<br>`);
+document.write(`<b>Math.trunc:</b> Redondea la cifra eliminando los decimales. Ejmeplo: 4.987867 = ${resultado = Math.trunc(4.987687)}`);
