e407d391eda093c4b9c72a63dfff1dd43a09a0f5
emmanuelvp88 <emmanuel.villalvap@gmail.com>
2023-06-27 23:00:53 -0600

    practicando la calculadora

--- 17-Historia de Cofla 4/index.js
+++ 17-Historia de Cofla 4/index.js
@@ -6,10 +6,25 @@ let operacion = prompt(`Que operacion deseas realizar:
 let num1 = prompt('Ingresa el primer numero.')
 let num2 = prompt('Ingresa el segundo numero.')
 let resultado;
+num1 = parseInt(num1)
+num2 = parseInt(num2)
 
 const suma = (num1, num2)=>{
-    resultado = num1 + num2;
-    return resultado;
+    return resultado = num1 + num2;
+}
+const resta = ()=>{
+    return resultado = num1 - num2 ;
+}
+const multiplicacion = ()=>{
+    return resultado = num1 * num2;
+}
+const divicion = ()=>{
+    return resultado = num1 / num2;
 }
 
-
+if (operacion == 1){
+    alert(`El resultado de tu suma es: ${suma(num1,num2)}`)
+}
+else if(operacion == 2){
+    alert(`El resultado de tu resta es: ${resta(num1, num2)}`)
+}
