8157d1f9ddf6f29f6a13bf8dd53063182e6ac45c
emmanuelvp88 <emmanuel.villalvap@gmail.com>
2023-06-27 21:48:42 -0600

    Insercion de leccion 4

--- 17-Historia de Cofla 4/index.js
+++ 17-Historia de Cofla 4/index.js
@@ -2,4 +2,14 @@ let operacion = prompt(`Que operacion deseas realizar:
                         1.- Suma 
                         2.- Resta
                         3.- Multiplicacion
-                        4.- Division`)
+                        4.- Division`);
+let num1 = prompt('Ingresa el primer numero.')
+let num2 = prompt('Ingresa el segundo numero.')
+let resultado;
+
+const suma = (num1, num2)=>{
+    resultado = num1 + num2;
+    return resultado;
+}
+
+
