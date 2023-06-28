dce3d0363b53f2cb34c87a41a524bbdef23110ca
emmanuelvp88 <emmanuel.villalvap@gmail.com>
2023-06-27 21:40:51 -0600

    Agrege contenido al .JS

---
+++ 17-Historia de Cofla 4/index.js
@@ -0,0 +1,5 @@
+let operacion = prompt(`Que operacion deseas realizar: 
+                        1.- Suma 
+                        2.- Resta
+                        3.- Multiplicacion
+                        4.- Division`)
