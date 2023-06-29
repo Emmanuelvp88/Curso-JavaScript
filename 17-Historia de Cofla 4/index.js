af24727e1954fdd17449ec754277b868a3e2c87f
emmanuelvp88 <emmanuel.villalvap@gmail.com>
2023-06-28 22:58:56 -0600

    en la leccion 17 subida de calcuadora

--- 17-Historia de Cofla 4/index.js
+++ 17-Historia de Cofla 4/index.js
@@ -3,37 +3,33 @@ let operacion = prompt(`Que operacion deseas realizar:
                         2.- Resta
                         3.- Multiplicacion
                         4.- Division`);
-let num1 = prompt('Ingresa el primer numero.')
-let num2 = prompt('Ingresa el segundo numero.')
+let num1 = prompt("Ingresa el primer numero.");
+let num2 = prompt("Ingresa el segundo numero.");
 let resultado;
-num1 = parseInt(num1)
-num2 = parseInt(num2)
+num1 = parseInt(num1);
+num2 = parseInt(num2);
 
-const suma = (num1, num2)=>{
-    return resultado = num1 + num2;
-}
-const resta = ()=>{
-    return resultado = num1 - num2 ;
-}
-const multiplicacion = ()=>{
-    return resultado = num1 * num2;
-}
-const divicion = ()=>{
-    return resultado = num1 / num2;
-}
+const suma = (num1, num2) => {
+    return (resultado = num1 + num2);
+};
+const resta = () => {
+    return (resultado = num1 - num2);
+};
+const multiplicacion = () => {
+    return (resultado = num1 * num2);
+};
+const divicion = () => {
+    return (resultado = num1 / num2);
+};
 
-if (operacion == 1){
-    alert(`El resultado de tu suma es: ${suma(num1,num2)}`)
-}
-else if(operacion == 2){
-    alert(`El resultado de tu resta es: ${resta(num1, num2)}`)
-}
-else if (operacion == 3){
+if (operacion == 1) {
+    alert(`El resultado de tu suma es: ${suma(num1, num2)}`);
+} else if (operacion == 2) {
+    alert(`El resultado de tu resta es: ${resta(num1, num2)}`);
+} else if (operacion == 3) {
     alert(`El resultado de tu multiplicacion es: ${multiplicacion(num1, num2)}`);
-}
-else if (operacion == 4){
+} else if (operacion == 4) {
     alert(`El resultado de tu divicion es: ${divicion(num1, num2)}`);
-}
-else{
+} else {
     alert(`Operacion no valida.`);
 }
