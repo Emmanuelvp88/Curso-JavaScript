dc4b55eb1ff8a109ad8a788e4ef4b7f4ac58fe8f
emmanuelvp88 <emmanuel.villalvap@gmail.com>
2023-07-23 22:09:18 -0600

    Agregando nuevas mejoras a la leccion 15

--- 15.-Metodos de Array/index.js
+++ 15.-Metodos de Array/index.js
@@ -1,25 +1,27 @@
 //METODOS DE UN ARRAY
                                 //METODOS TRASFORMADORES
-document.write("<b>pop(): </b> Quita el ultimo elemento de un array y lo devuenve en una variable <br>");
 let array = [" jorge ", " pedro ", " juan "]
-document.write(`Array original: <b>${array}</b> <br>`);
-document.write(`Devolviendo el ultimo elemento del array con "pop": <b> ${array.pop()}</b> <br>`);
+document.write(`Array original: <b>${array}</b> <br><br>`);
+
+document.write("<b>pop(): </b> Quita el ultimo elemento de un array y lo devuenve en una variable <br>");
 let ultimoElemento = array.pop();
-document.write(`Mostramos el ultimo elemento con "pop": <b>${array}</b><br>`);
-document.write(`Sobrante del Array: <b>${array}</b> <br>`)
-//Recordemos que el metodo "pop" va quitando el ultimo elemento del array como actualmente esta
-//Es por eso que al final de todo el array solo nos quedo con un solo elemento
+document.write(`Devolviendo el ultimo elemento del array con "pop": <b> ${ultimoElemento}</b> <br><br>`);
+document.write(`Sobrante del Array: <b>${array}</b><br>`)
+
+document.write('Recordemos que el metodo "pop" va quitando el ultimo elemento del array como actualmente esta <br><br>')
+//Es por eso que al final de todo el array solo nos quedan dos elementos
 
 document.write(`<b>shift(): </b> Igual que "pop" solo que con el elemento del principio.<br>`);
 array.push(" emmanuel", " carlos");
-document.write(`<b>push(): </b> Agregra un elemento al array al final de la lista: <b>${array}</b> <br> `);
+document.write(`<b>push(): </b> Agrega elementos al array al final de la lista: <b>${array}</b> <br> `);
 document.write(`<b>reverse(): </b> invierte el orden de los elementos de un array: <b>${array.reverse()}</b> <br> `);
-array.unshift("pablo", "emiliano")
+array.unshift("pablo", "lazaro")
 document.write(`<b>unshift(): </b> Agraga uno o mas elementos al principio de un array y devuelve la nueva longitud del array. <b>${array}</b><br>`);
-document.write(`<b>sort(): </b> Ordenas los elementos de un array localmente y los devuelve ya ordenados en el alfabeto lexicografico: <b>${array.sort()}</b><br>`);
+document.write(`<b>sort(): </b> Ordenas los elementos de un array localmente y los devuelve ya ordenados en el alfabeto lexicografico: <b>${array.sort()}</b><br><br>`);
 //Los primeros dos parametros que le pasamos a "splice()" sirven para mandarle aquitar elementos y los demas para agregar 
 array.splice(0, 2, "Suerte", "para", 3)
 document.write(`<b>splice(): </b> Cambia el contenido de un array eliminando elementos y/o agregando nuevos. <b>: ${array}</b><br>`);
+document.write('indicamos que eliminara los elementos del 0 al 2  y agregara otros 3 elemetos<br><br>');
 
                                 //METODOS ACCESORES
 document.write(`<b>join(): </b>Une los elementos de un array, y entremedio de cada elemento ponemos cualquier caracter que le indiquemos y lo devuelve como una sola cadena de texto.<b>${array.join(" - ")}</b><br>`);
