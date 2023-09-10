dfaf8dcf774d1d04d8c054af05744f133e7530dc
emmanuelvp88 <emmanuel.villalvap@gmail.com>
2023-09-10 17:40:09 -0600

    agrege mensaje sobre la consola web

---
+++ 18-Metodos de Registro en Consola/index.js
@@ -0,0 +1,7 @@
+document.write('<h3 style = "font-size: 2em; color: blue; text-align: center" >Metodoos de registor en Consola</h3>');
+document.write(`- assert() -: aparece un mensaje de error en la consola si la afirmacion es falsa. si la afirmacion es verdadera, no aparecera nada. (NO ESTANDAR)<br>
+- clear() -: Limpia la consola. <br>
+- error() -: muestra un error en la consola web. <br>
+- info() -: emite un mensaje de informacion a la consola web. en firefox y crome se muestra un pequeño icono "i" justo a estos elementos en el registro de la consola web.<br>
+`);
+document.write('');
